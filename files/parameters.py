46e68da609de1dcb0d2cbc382540a72e4a0ef8bb
Jaime Sánchez Claros <88107340+jclaros27@users.noreply.github.com>
2023-02-08 18:07:52 +0100

    parameters.py modified
    Modification in parameters.py to include the new labels of the synapses object

--- files/parameters.py
+++ files/parameters.py
@@ -47,19 +47,19 @@ nsyns_neurons_neurons["cck_ca1_to_pyr_ca1"] = [25,25] # to optimized # esto hay 
 # nsyns_neurons_neurons["pyr_ca3_to_cck_ca1"] = [100] # to be optimized
 
 # Internal CA3
-syn_neurons_neurons["pyr_ca3_to_pyr_ca3"] = [["BdendAMPAf","BdendNMDA"]]
-syn_neurons_neurons["bas_ca3_to_pyr_ca3"] = [["somaGABAf"]]
-syn_neurons_neurons["olm_ca3_to_pyr_ca3"] = [["Adend3GABAfb"]]
-syn_neurons_neurons["pyr_ca3_to_bas_ca3"] = [["somaAMPAf", "somaNMDA"]]
-syn_neurons_neurons["bas_ca3_to_bas_ca3"] = [["somaGABAf"]]
-syn_neurons_neurons["pyr_ca3_to_olm_ca3"] = [["somaAMPAf","somaNMDA"]]
+syn_neurons_neurons["pyr_ca3_to_pyr_ca3"] = [["BdendAMPA_pyr","BdendNMDA_pyr"]]  # [["BdendAMPAf","BdendNMDA"]]
+syn_neurons_neurons["bas_ca3_to_pyr_ca3"] = [["somaGABA_bas"]]                   # [["somaGABA"]]
+syn_neurons_neurons["olm_ca3_to_pyr_ca3"] = [["Adend3GABA_olm"]]                 # [["Adend3GABAfb"]]
+syn_neurons_neurons["pyr_ca3_to_bas_ca3"] = [["somaAMPA_pyr","somaNMDA_pyr"]]    # [["somaAMPAf", "somaNMDA"]]
+syn_neurons_neurons["bas_ca3_to_bas_ca3"] = [["somaGABA_bas"]]                   # [["somaGABAf"]]
+syn_neurons_neurons["pyr_ca3_to_olm_ca3"] = [["somaAMPA_pyr","somaNMDA_pyr"]     # [["somaAMPAf","somaNMDA"]]
 # Internal CA1
-syn_neurons_neurons["bas_ca1_to_pyr_ca1"] = [["somaGABAf"]]
-syn_neurons_neurons["olm_ca1_to_pyr_ca1"] = [["Adend3GABAfb"]]
-syn_neurons_neurons["pyr_ca1_to_bas_ca1"] = [["somaAMPAf", "somaNMDA"]]
-syn_neurons_neurons["bas_ca1_to_bas_ca1"] = [["somaGABAf"]]
-syn_neurons_neurons["pyr_ca1_to_olm_ca1"] = [["somaAMPAf","somaNMDA"]]
-syn_neurons_neurons["cck_ca1_to_pyr_ca1"] = [["Adend2GABAf", "somaGABAf"]] # updated on
+syn_neurons_neurons["bas_ca1_to_pyr_ca1"] = [["somaGABA_pyr"]]                   # [["somaGABAf"]]
+syn_neurons_neurons["olm_ca1_to_pyr_ca1"] = [["Adend3GABA_olm"]                  # [["Adend3GABAfb"]]
+syn_neurons_neurons["pyr_ca1_to_bas_ca1"] = [["somaAMPA_bas","somaNMDA_bas"]]    # [["somaAMPAf", "somaNMDA"]]
+syn_neurons_neurons["bas_ca1_to_bas_ca1"] = [["somaGABA_bas"]]                   # [["somaGABAf"]]
+syn_neurons_neurons["pyr_ca1_to_olm_ca1"] = [["somaAMPA_pyr","somaNMDA_pyr"]]    # [["somaAMPAf","somaNMDA"]]
+syn_neurons_neurons["cck_ca1_to_pyr_ca1"] = [["Adend2GABA_cck", "somaGABA_cck"]] # [["Adend2GABAf", "somaGABAf"]] # updated on
 # Schaffer colaterals
 # syn_neurons_neurons["pyr_ca3_to_pyr_ca1"] = [["Adend1AMPAf","Adend1NMDA"]]
 # syn_neurons_neurons["pyr_ca3_to_bas_ca1"] = [["somaAMPAf","somaNMDA"]]
@@ -137,25 +137,25 @@ nsyns_inputs_neurons["ec3_360_to_bas_ca1"] = [25]
 nsyns_inputs_neurons["ec3_360_to_cck_ca1"] = [25]
 
 # to CA3
-syn_inputs_neurons["sep_180_to_bas_ca3"] = [["somaGABAf"]]
-syn_inputs_neurons["sep_360_to_olm_ca3"] = [["somaGABAf"]]
-syn_inputs_neurons["ec2_180_to_pyr_ca3"] = [["Adend3AMPAf","Adend3NMDA"]]# AMPA/ NMDA
-syn_inputs_neurons["ec2_180_to_bas_ca3"] = [["somaAMPAf", "somaNMDA"]]
-syn_inputs_neurons["ec2_360_to_pyr_ca3"] = [["Adend3AMPAf","Adend3NMDA"]]
-syn_inputs_neurons["ec2_360_to_bas_ca3"] = [["somaAMPAf", "somaNMDA"]]
-syn_inputs_neurons["dg_regular_to_pyr_ca3"] = [["Adend1AMPAf","Adend1NMDA"]]
-syn_inputs_neurons["dg_regular_to_bas_ca3"] = [["somaAMPAf", "somaNMDA"]]
-syn_inputs_neurons["dg_burst_to_pyr_ca3"] = [["Adend1AMPAf","Adend1NMDA"]]
-syn_inputs_neurons["dg_burst_to_bas_ca3"] = [["somaAMPAf", "somaNMDA"]]
+syn_inputs_neurons["sep_180_to_bas_ca3"]    = [["somaGABA_sep180"]]                          # [["somaGABAf"]]
+syn_inputs_neurons["sep_360_to_olm_ca3"]    = [["somaGABA_sep360"]]                          # [["somaGABAf"]]
+syn_inputs_neurons["ec2_180_to_pyr_ca3"]    = [["Adend3AMPA_ec2180", "Adend3NMDA_ec2180"]]   # [["Adend3AMPAf","Adend3NMDA"]]
+syn_inputs_neurons["ec2_180_to_bas_ca3"]    = [["somaAMPA_ec2180",   "somaNMDA_ec2180"]]     # [["somaAMPAf", "somaNMDA"]]
+syn_inputs_neurons["ec2_360_to_pyr_ca3"]    = [["Adend3AMPA_ec2360", "Adend3NMDA_ec2"]]      # [["Adend3AMPAf","Adend3NMDA"]]
+syn_inputs_neurons["ec2_360_to_bas_ca3"]    = [["somaAMPA_ec2360",   "somaNMDA_ec2360"]]     # [["somaAMPAf", "somaNMDA"]]
+syn_inputs_neurons["dg_regular_to_pyr_ca3"] = [["Adend1AMPA_dgreg","Adend1NMDA_dgreg"]]      # [["Adend1AMPAf","Adend1NMDA"]]
+syn_inputs_neurons["dg_regular_to_bas_ca3"] = [["somaAMPA_dgreg","somaNMDA_dgreg"]]          # [["somaAMPAf", "somaNMDA"]]
+syn_inputs_neurons["dg_burst_to_pyr_ca3"]   = [["Adend1AMPA_dgburst","Adend1NMDA_dgburst"]]  # [["Adend1AMPAf","Adend1NMDA"]]
+syn_inputs_neurons["dg_burst_to_bas_ca3"]   = [["somaAMPA_dgburst", "somaNMDA_dgburst"]]     # [["somaAMPAf", "somaNMDA"]]
 # to CA1
-syn_inputs_neurons["sep_180_to_bas_ca1"] = [["somaGABAf"]]
-syn_inputs_neurons["sep_360_to_olm_ca1"] = [["somaGABAf"]]
-syn_inputs_neurons["ec3_180_to_pyr_ca1"] = [["Adend3AMPAf", "Adend3NMDA"]]
-syn_inputs_neurons["ec3_180_to_bas_ca1"] = [["somaAMPAf", "somaNMDA"]]
-syn_inputs_neurons["ec3_180_to_cck_ca1"] = [["somaAMPAf", "somaNMDA"]]
-syn_inputs_neurons["ec3_360_to_pyr_ca1"] = [["Adend3AMPAf", "Adend3NMDA"]]
-syn_inputs_neurons["ec3_360_to_bas_ca1"] = [["somaAMPAf", "somaNMDA"]]
-syn_inputs_neurons["ec3_360_to_cck_ca1"] = [["somaAMPAf", "somaNMDA"]]
+syn_inputs_neurons["sep_180_to_bas_ca1"] = [["somaGABA_sep180"]]                             # [["somaGABAf"]]
+syn_inputs_neurons["sep_360_to_olm_ca1"] = [["somaGABA_sep360"]]                             # [["somaGABAf"]]
+syn_inputs_neurons["ec3_180_to_pyr_ca1"] = [["Adend3AMPA_ec3180","Adend3NMDA_ec3180"]]       # [["Adend3AMPAf", "Adend3NMDA"]]
+syn_inputs_neurons["ec3_180_to_bas_ca1"] = [["somaAMPA_ec3180","somaNMDA_ec3180"]]           # [["somaAMPAf", "somaNMDA"]]
+syn_inputs_neurons["ec3_180_to_cck_ca1"] = [["somaAMPA_ec3180","somaNMDA_ec3180"]]           # [["somaAMPAf", "somaNMDA"]]
+syn_inputs_neurons["ec3_360_to_pyr_ca1"] = [["Adend3AMPA_ec3360","Adend3NMDA_ec3360"]]       # [["Adend3AMPAf", "Adend3NMDA"]]
+syn_inputs_neurons["ec3_360_to_bas_ca1"] = [["somaAMPA_ec3360","somaNMDA_ec3360"]]           # [["somaAMPAf", "somaNMDA"]]
+syn_inputs_neurons["ec3_360_to_cck_ca1"] = [["somaAMPA_ec3360","somaNMDA_ec3360"]]           # [["somaAMPAf", "somaNMDA"]]
 
 # to CA3 # added in 15/11
 delay_inputs_neurons["sep_180_to_bas_ca3"] = [2.0,1.0]
@@ -188,35 +188,55 @@ weights_noise_neurons = dict.fromkeys(keys)
 for key in keys:
     weights_noise_neurons[key] = {}
 
-weights_noise_neurons["pyr_ca3"]["somaAMPAf"]   = 0.05e-3
-weights_noise_neurons["pyr_ca3"]["somaGABAf"]   = 0.012e-3
+# weights_noise_neurons["pyr_ca3"]["somaAMPAf"]   = 0.05e-3
+# weights_noise_neurons["pyr_ca3"]["somaGABAf"]   = 0.012e-3
+weights_noise_neurons["pyr_ca3"]["somaAMPA_noise"]   = 0.05e-3
+weights_noise_neurons["pyr_ca3"]["somaGABA_noise"]   = 0.012e-3
+                                             
+# weights_noise_neurons["pyr_ca3"]["Adend3AMPAf"] = 2*0.05e-3
+# weights_noise_neurons["pyr_ca3"]["Adend3GABAf"] = 0.012e-3
+# weights_noise_neurons["pyr_ca3"]["Adend3NMDA"]  = 0.0
+weights_noise_neurons["pyr_ca3"]["Adend3AMPA_noise"] = 2*0.05e-3
+weights_noise_neurons["pyr_ca3"]["Adend3GABA_noise"] = 0.012e-3
+# weights_noise_neurons["pyr_ca3"]["Adend3NMDA_noise"] = 0.0
+                                             
+# weights_noise_neurons["bas_ca3"]["somaAMPAf"]   = 0.02e-3
+# weights_noise_neurons["bas_ca3"]["somaGABAf"]   = 0.2e-3
+weights_noise_neurons["bas_ca3"]["somaAMPA_noise"]   = 0.02e-3
+weights_noise_neurons["bas_ca3"]["somaGABA_noise"]   = 0.2e-3
+                                             
+# weights_noise_neurons["olm_ca3"]["somaAMPAf"]   = 0.0625e-3
+# weights_noise_neurons["olm_ca3"]["somaGABAf"]   = 0.2e-3
+weights_noise_neurons["olm_ca3"]["somaAMPA_noise"]   = 0.0625e-3
+weights_noise_neurons["olm_ca3"]["somaGABA_noise"]   = 0.2e-3
+                                             
+# weights_noise_neurons["pyr_ca1"]["somaAMPAf"]   = 0.05e-3
+# weights_noise_neurons["pyr_ca1"]["somaGABAf"]   = 0.012e-3
+weights_noise_neurons["pyr_ca1"]["somaAMPA_noise"]   = 0.05e-3
+weights_noise_neurons["pyr_ca1"]["somaGABA_noise"]   = 0.012e-3
 
-weights_noise_neurons["pyr_ca3"]["Adend3AMPAf"] = 2*0.05e-3
-weights_noise_neurons["pyr_ca3"]["Adend3GABAf"] = 0.012e-3
-weights_noise_neurons["pyr_ca3"]["Adend3NMDA"]  = 0.0
+# weights_noise_neurons["pyr_ca1"]["Adend3AMPAf"] = 2*0.05e-3
+# weights_noise_neurons["pyr_ca1"]["Adend3GABAf"] = 0.012e-3
+# weights_noise_neurons["pyr_ca1"]["Adend3NMDA"]  = 0.0
+weights_noise_neurons["pyr_ca1"]["Adend3AMPA_noise"] = 2*0.05e-3
+weights_noise_neurons["pyr_ca1"]["Adend3GABA_noise"] = 0.012e-3
+# weights_noise_neurons["pyr_ca1"]["Adend3NMDA_noise"]  = 0.0
 
-weights_noise_neurons["bas_ca3"]["somaAMPAf"]   = 0.02e-3
-weights_noise_neurons["bas_ca3"]["somaGABAf"]   = 0.2e-3
-
-weights_noise_neurons["olm_ca3"]["somaAMPAf"]   = 0.0625e-3
-weights_noise_neurons["olm_ca3"]["somaGABAf"]   = 0.2e-3
-
-weights_noise_neurons["pyr_ca1"]["somaAMPAf"]   = 0.05e-3
-weights_noise_neurons["pyr_ca1"]["somaGABAf"]   = 0.012e-3
-
-weights_noise_neurons["pyr_ca1"]["Adend3AMPAf"] = 2*0.05e-3
-weights_noise_neurons["pyr_ca1"]["Adend3GABAf"] = 0.012e-3
-weights_noise_neurons["pyr_ca1"]["Adend3NMDA"]  = 0.0
-
-weights_noise_neurons["bas_ca1"]["somaAMPAf"]   = 0.02e-3
-weights_noise_neurons["bas_ca1"]["somaGABAf"]   = 0.2e-3
-
-weights_noise_neurons["olm_ca1"]["somaAMPAf"]   = 0.0625e-3
-weights_noise_neurons["olm_ca1"]["somaGABAf"]   = 0.2e-3
-
-weights_noise_neurons["cck_ca1"]["somaAMPAf"]   = 0.045e-3 # updated on 15/11/2022
-weights_noise_neurons["cck_ca1"]["somaGABAf"]   = 0.012e-3
-
+# weights_noise_neurons["bas_ca1"]["somaAMPAf"]   = 0.02e-3
+# weights_noise_neurons["bas_ca1"]["somaGABAf"]   = 0.2e-3
+weights_noise_neurons["bas_ca1"]["somaAMPA_noise"]   = 0.02e-3
+weights_noise_neurons["bas_ca1"]["somaGABA_noise"]   = 0.2e-3
+                                             
+# weights_noise_neurons["olm_ca1"]["somaAMPAf"]   = 0.0625e-3
+# weights_noise_neurons["olm_ca1"]["somaGABAf"]   = 0.2e-3
+weights_noise_neurons["olm_ca1"]["somaAMPA_noise"]   = 0.0625e-3
+weights_noise_neurons["olm_ca1"]["somaGABA_noise"]   = 0.2e-3
+                                             
+# weights_noise_neurons["cck_ca1"]["somaAMPAf"]   = 0.045e-3 # updated on 15/11/2022
+# weights_noise_neurons["cck_ca1"]["somaGABAf"]   = 0.012e-3
+weights_noise_neurons["cck_ca1"]["somaAMPA_noise"]   = 0.045e-3 # updated on 15/11/2022
+weights_noise_neurons["cck_ca1"]["somaGABA_noise"]   = 0.012e-3
+                                             
 '''
 print("Making Background Noise")
 rdtmp = 0 # add_to_sead value - incremented in make_NetStims
