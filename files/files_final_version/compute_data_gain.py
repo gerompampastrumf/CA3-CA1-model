0e7dd13947423caa52b01296cc5f459671407955
Jaime Sánchez Claros <88107340+jclaros27@users.noreply.github.com>
2023-10-30 08:23:22 +0100

    Update compute_data_gain.py
    Minor adjustments

--- files/files_final_version/compute_data_gain.py
+++ files/files_final_version/compute_data_gain.py
@@ -18,89 +18,89 @@ path = '/home/jaime/Desktop/hippocampus/external_inputs/baseline/'
 path_ca3 = os.path.join(path, folders[int(sys.argv[1])])
 path_ca1 = os.path.join(path, folders[int(sys.argv[1])]) #'/home/jaime/Desktop/hippocampus/external_inputs/'#os.path.join(path,'baseline') # they will be different in a specific cases.. where ctually ca3 would be already computed
 
-# print("Computing PSDs CA3 ...")
-# try:
-#     print("... from voltages")
-#     cp.compute_psd_volt_ca3(path_ca3)
-#     print("... from LFPs")
-#     cp.compute_psd_lfp_ca3(path_ca3)
-#     print("... from ICAs")
-#     cp.compute_psd_ica_ca3(path_ca3)
+print("Computing PSDs CA3 ...")
+try:
+    print("... from voltages")
+    cp.compute_psd_volt_ca3(path_ca3)
+    print("... from LFPs")
+    cp.compute_psd_lfp_ca3(path_ca3)
+    print("... from ICAs")
+    cp.compute_psd_ica_ca3(path_ca3)
+    print("... from synaptic currents")
+    cp.compute_psd_synaptic_currents_ca3(path_ca3)
+except FileNotFoundError: 
+    print("... no CA3 files in this folder")
+
+print("Computing PSDs CA1 ...")
+try:
+    print("... from voltages")
+    cp.compute_psd_volt_ca1(path_ca1)
+    print("... from LFPs")
+    cp.compute_psd_lfp_ca1(path_ca1)
+    print("... from ICAs")
+    cp.compute_psd_ica_ca1(path_ca1)
+    print("... from synaptic currents")
+    cp.compute_psd_synaptic_currents_ca1(path_ca1)
+    print(" ... done")
+    print(" ")
+except FileNotFoundError: 
+    print("... no CA1 files in this folder")
+
+# Compute phases
+print("Computing phases CA3 ...")
+try:
+    cpfs.compute_phases_from_spikes_ca3(path_ca3)
+except FileNotFoundError: 
+    print("... no CA3 files in this folder")
+print("Computing phases CA1 ...")
+try:
+    cpfs.compute_phases_from_spikes_ca1(path_ca1)
+    print(" ... done")
+except FileNotFoundError: 
+    print("... no CA1 files in this folder")
+print(" ")
+
+print("Computing spiking patterns")
+csp.compute_spiking_patterns_with_dynamic_reference(path_ca3,path_ca1)
+
+# Compute theta-gamma coupling
+print("Computing theta-gamma coupling CA3 ...")
+try:
+    print("... from voltages")
+    ctgc.compute_amplitude_coupling_from_volt(path_ca3, input_filename="volt_pyr_ca3.lzma",output_filename="volt_pyr_ca3_amplitude_coupling.lzma")
+    print("... from LFPs")
+    ctgc.compute_amplitude_coupling_from_lfp(path_ca3, input_filename="lfp_ca3.lzma", output_filename="lfp_ca3_amplitude_coupling.lzma")
+    print("... from ICAs")
+    ctgc.compute_amplitude_coupling_from_ica(path_ca3, input_filename="ica_ca3.lzma", output_filename="ica_ca3_amplitud_coupling.lzma")
 #     print("... from synaptic currents")
-#     cp.compute_psd_synaptic_currents_ca3(path_ca3)
-# except FileNotFoundError: 
-#     print("... no CA3 files in this folder")
+#     ctgc.compute_amplitude_coupling_from_synaptic_current_ca3(path_ca3)
+except FileNotFoundError: 
+    print("... no CA3 files in this folder")
 
-# print("Computing PSDs CA1 ...")
-# try:
-#     print("... from voltages")
-#     cp.compute_psd_volt_ca1(path_ca1)
-#     print("... from LFPs")
-#     cp.compute_psd_lfp_ca1(path_ca1)
-#     print("... from ICAs")
-#     cp.compute_psd_ica_ca1(path_ca1)
-#     print("... from synaptic currents")
-#     cp.compute_psd_synaptic_currents_ca1(path_ca1)
-#     print(" ... done")
-#     print(" ")
-# except FileNotFoundError: 
-#     print("... no CA1 files in this folder")
-
-# # Compute phases
-# print("Computing phases CA3 ...")
-# try:
-#     cpfs.compute_phases_from_spikes_ca3(path_ca3)
-# except FileNotFoundError: 
-#     print("... no CA3 files in this folder")
-# print("Computing phases CA1 ...")
-# try:
-#     cpfs.compute_phases_from_spikes_ca1(path_ca1)
-#     print(" ... done")
-# except FileNotFoundError: 
-#     print("... no CA1 files in this folder")
-# print(" ")
-
-# print("Computing spiking patterns")
-# csp.compute_spiking_patterns_with_dynamic_reference(path_ca3,path_ca1)
-
-# # Compute theta-gamma coupling
-# print("Computing theta-gamma coupling CA3 ...")
-# try:
-#     print("... from voltages")
-#     ctgc.compute_amplitude_coupling_from_volt(path_ca3, input_filename="volt_pyr_ca3.lzma",output_filename="volt_pyr_ca3_amplitude_coupling.lzma")
-#     print("... from LFPs")
-#     ctgc.compute_amplitude_coupling_from_lfp(path_ca3, input_filename="lfp_ca3.lzma", output_filename="lfp_ca3_amplitude_coupling.lzma")
-#     print("... from ICAs")
-#     ctgc.compute_amplitude_coupling_from_ica(path_ca3, input_filename="ica_ca3.lzma", output_filename="ica_ca3_amplitud_coupling.lzma")
-# #     print("... from synaptic currents")
-# #     ctgc.compute_amplitude_coupling_from_synaptic_current_ca3(path_ca3)
-# except FileNotFoundError: 
-#     print("... no CA3 files in this folder")
-
-# print("Computing theta-gamma coupling CA1 ...")
-# try:
-#     print("... from voltages")
-#     ctgc.compute_amplitude_coupling_from_volt(path_ca1,input_filename="volt_pyr_ca1.lzma",output_filename="volt_pyr_ca1_amplitude_coupling.lzma")
-#     print("... from LFPs")
-#     ctgc.compute_amplitude_coupling_from_lfp(path_ca1, input_filename="lfp_ca1.lzma", output_filename="lfp_ca1_amplitude_coupling.lzma")
-#     print("... from ICAs")
-#     ctgc.compute_amplitude_coupling_from_ica(path_ca1, input_filename="ica_ca1.lzma", output_filename="ica_ca1_amplitude_coupling.lzma")
-#     # print("... from synaptic currents")
-#     # ctgc.compute_amplitude_coupling_from_synaptic_current_ca1(path_ca1)
-#     print(" ... done")
-#     print(" ")
-# except FileNotFoundError:
-#     print("... no CA1 files in this folder")
-#     print(" ")
+print("Computing theta-gamma coupling CA1 ...")
+try:
+    print("... from voltages")
+    ctgc.compute_amplitude_coupling_from_volt(path_ca1,input_filename="volt_pyr_ca1.lzma",output_filename="volt_pyr_ca1_amplitude_coupling.lzma")
+    print("... from LFPs")
+    ctgc.compute_amplitude_coupling_from_lfp(path_ca1, input_filename="lfp_ca1.lzma", output_filename="lfp_ca1_amplitude_coupling.lzma")
+    print("... from ICAs")
+    ctgc.compute_amplitude_coupling_from_ica(path_ca1, input_filename="ica_ca1.lzma", output_filename="ica_ca1_amplitude_coupling.lzma")
+    # print("... from synaptic currents")
+    # ctgc.compute_amplitude_coupling_from_synaptic_current_ca1(path_ca1)
+    print(" ... done")
+    print(" ")
+except FileNotFoundError:
+    print("... no CA1 files in this folder")
+    print(" ")
 
 print("Computing cross theta-gamma coupling with soma pyr CA1 as reference ...")
 try:
-    # print("... from voltages")
-    # ctgcr.compute_amplitude_coupling_from_volt(folder_ca3=path_ca3,folder_ca1=path_ca1,input_filename_ca3="volt_pyr_ca3.lzma",input_filename_ca1="volt_pyr_ca1.lzma", output_filename = "volt_pyr_amplitude_coupling_reference.lzma")
-    # print("...ok")
-    # print("... from LFPs")
-    # ctgcr.compute_amplitude_coupling_from_lfp(folder_ca3=path_ca3,folder_ca1=path_ca1,input_filename_ca3="lfp_ca3.lzma",input_filename_ca1="lfp_ca1.lzma", output_filename = "lfp_amplitude_coupling_reference.lzma")
-    # print("...ok")
+    print("... from voltages")
+    ctgcr.compute_amplitude_coupling_from_volt(folder_ca3=path_ca3,folder_ca1=path_ca1,input_filename_ca3="volt_pyr_ca3.lzma",input_filename_ca1="volt_pyr_ca1.lzma", output_filename = "volt_pyr_amplitude_coupling_reference.lzma")
+    print("...ok")
+    print("... from LFPs")
+    ctgcr.compute_amplitude_coupling_from_lfp(folder_ca3=path_ca3,folder_ca1=path_ca1,input_filename_ca3="lfp_ca3.lzma",input_filename_ca1="lfp_ca1.lzma", output_filename = "lfp_amplitude_coupling_reference.lzma")
+    print("...ok")
 
     print("... from ICAs")
     ctgcr.compute_amplitude_coupling_from_ica(folder_ca3=path_ca3,folder_ca1=path_ca1,input_filename_ca3="ica_ca3.lzma",input_filename_ca1="ica_ca1.lzma", output_filename = "ica_amplitude_coupling_reference.lzma")
